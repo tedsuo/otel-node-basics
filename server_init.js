25b4ca5aab564a1d3656f3f5f3769dd055f35105
Ted Young <ted@lightstep.com>
2020-11-29 17:53:56 -0800

    better comments

--- server_init.js
+++ server_init.js
@@ -1,23 +1,30 @@
 const { lightstep } = require('lightstep-opentelemetry-launcher-node');
 
+// This file replaces your application's original start script,
+// allowing OpenTelelmetry to load before any other modules.
+// Change './server' to the path of your actual start script.
+const startScript = './server';
+
 // Use the launcher to set up and return the OpenTelemetry NodeSDK.
 // Recommended parameters:
-// accessToken: required, can be found in Lightstep settings.
-// serviceName: Required, for comparision across service instances.
-// serviceVersion: Highly recommended for identifing regressions.
-// propagators: tracecontext highly recommended. Also include b3 if you need to
-//              transition from b3 headers to tracecontext headers.
+// * accessToken: required, found in Lightstep settings.
+// * serviceName: required, used to identify resources.
+// * serviceVersion: highly recommended, used to identify regressions.
+// * propagators: tracecontext highly recommended. Also include b3 if you 
+//                need to transition from b3 headers to tracecontext headers.
 const sdk = lightstep.configureOpenTelemetry({
   accessToken: '<ACCESS TOKEN>',
   serviceName: 'hello-server',
   serviceVersion: 'v1.2.3',
-  propagators: 'tracecontext,b3',
+  propagators: 'tracecontext,b3,baggage',
 });
 
 // start the sdk and wait for any installed resource detection to run.
+// For auto-instrumentation to work, the SDK must be started before any 
+// other packages are loaded.
 sdk.start().then(() => {
-  // require your application startup file here.
-  require('./server');
+  // require your application once opentelemetry has loaded.
+  require(startScript);
 });
 
 // Shutdown flushes any remaining spans before exit. In future versions,
